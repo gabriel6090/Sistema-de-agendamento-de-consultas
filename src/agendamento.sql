b9ec01a86f9ecf744b9c60e6d662ae95df16115d
gabriel6090 <51978777+gabriel6090@users.noreply.github.com>
2025-05-24 23:11:51 -0300

    Update agendamento.sql

--- src/agendamento.sql
+++ src/agendamento.sql
@@ -1,13 +1,13 @@
--- MySQL dump 10.13  Distrib 8.0.41, for Win64 (x86_64)
+-- MySQL dump 10.13  Distrib 8.0.42, for Linux (x86_64)
 --
 -- Host: localhost    Database: agendamento
 -- ------------------------------------------------------
--- Server version	8.0.41
+-- Server version	8.0.42-0ubuntu0.24.04.1
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!50503 SET NAMES utf8 */;
+/*!50503 SET NAMES utf8mb4 */;
 /*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
 /*!40103 SET TIME_ZONE='+00:00' */;
 /*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
@@ -23,16 +23,17 @@ DROP TABLE IF EXISTS `consultas`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `consultas` (
-  `id` int NOT NULL AUTO_INCREMENT,
-  `data_hora` datetime NOT NULL,
+  `id_consulta` int NOT NULL AUTO_INCREMENT,
+  `data` date NOT NULL,
+  `hora` time NOT NULL,
   `paciente_id` int NOT NULL,
   `medico_id` int NOT NULL,
-  PRIMARY KEY (`id`),
+  PRIMARY KEY (`id_consulta`),
   KEY `paciente_id` (`paciente_id`),
   KEY `medico_id` (`medico_id`),
   CONSTRAINT `consultas_ibfk_1` FOREIGN KEY (`paciente_id`) REFERENCES `pacientes` (`id`),
   CONSTRAINT `consultas_ibfk_2` FOREIGN KEY (`medico_id`) REFERENCES `medicos` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=18 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -41,6 +42,7 @@ CREATE TABLE `consultas` (
 
 LOCK TABLES `consultas` WRITE;
 /*!40000 ALTER TABLE `consultas` DISABLE KEYS */;
+INSERT INTO `consultas` VALUES (2,'2025-05-24','15:30:00',10,2),(4,'2025-05-26','13:04:00',9,3),(5,'2025-05-24','13:00:00',4,2),(7,'2025-06-20','15:00:00',15,3),(9,'2025-05-30','15:00:00',16,1),(10,'2025-05-24','10:00:00',4,2),(12,'2005-12-24','16:00:00',19,1),(13,'2025-05-24','09:15:00',4,3),(15,'2025-05-24','13:15:00',4,3),(16,'2025-07-01','10:05:00',25,1),(17,'1986-09-30','12:30:00',32,1);
 /*!40000 ALTER TABLE `consultas` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -88,7 +90,7 @@ CREATE TABLE `pacientes` (
   `senha` varchar(255) NOT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `cpf` (`cpf`)
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=34 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -97,7 +99,7 @@ CREATE TABLE `pacientes` (
 
 LOCK TABLES `pacientes` WRITE;
 /*!40000 ALTER TABLE `pacientes` DISABLE KEYS */;
-INSERT INTO `pacientes` VALUES (1,'João da Silva','1985-07-15','12345678901','senha123'),(2,'Maria Oliveira','1990-11-22','98765432109','senha456'),(3,'Carlos Souza','1978-03-10','45612378905','senha789');
+INSERT INTO `pacientes` VALUES (1,'João da Silva','1985-07-15','12345678901','senha123'),(2,'Maria Oliveira','1990-11-22','98765432109','senha456'),(3,'Carlos Souza','1978-03-10','45612378905','senha789'),(4,'sa','2003-01-20','12345678911','1234'),(7,'Rodrigo','2008-05-20','12345678922','1234'),(8,'as','2005-01-20','12345678933','1234'),(9,'Jeane','1998-01-20','12345678944','1234'),(10,'Gabriel','0024-05-20','12345678955','1234'),(12,'lais','0200-01-20','12345678912','1234'),(13,'Executavel','2004-01-20','12345678913','1234'),(14,'RENAN','2000-10-10','12345678910','2010'),(15,'Carlos','2000-10-27','36568947301','123456'),(16,'victor gusmão','2005-11-15','44585206876','Victor15@'),(18,'jj','2010-10-10','10987654321','123senha'),(19,'Miguel Silva de Sousa','2005-07-04','12345678999','Indio0407'),(20,'Kaique Mocelin','2005-10-04','99900088876','0000'),(25,'Tauan','1997-06-07','12345678915','tauan'),(28,'alex','2004-07-17','12345698711','1236'),(29,'nidal f','2005-07-27','12345678988','1456'),(32,'mop','1986-08-30','78945612301','123456'),(33,'nidal fares silva','2005-07-28','12345678966','6868');
 /*!40000 ALTER TABLE `pacientes` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -110,4 +112,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2025-05-15 23:20:45
+-- Dump completed on 2025-05-24 12:28:17
